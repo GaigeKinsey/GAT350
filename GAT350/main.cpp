0e04d5c45d5a02eedff70ffddbe0e6382fcb5ab0
Gaige Kinsey <gaigekinsey@yahoo.com>
2019-10-18 11:57:12 -0600

    Added the lighting and colors

--- GAT350/main.cpp
+++ GAT350/main.cpp
@@ -6,42 +6,52 @@
 #include "engine/renderer/vertex_index_array.h"
 #include "engine/renderer/texture.h"
 
-const GLfloat cube_vertices[] = {
-	// front
-	-1.0, -1.0,  1.0, 1.0, 0.0, 0.0, 0.0, 0.0,
-	 1.0, -1.0,  1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
-	 1.0,  1.0,  1.0, 0.0, 0.0, 1.0, 1.0, 1.0,
-	-1.0,  1.0,  1.0, 1.0, 1.0, 1.0, 0.0, 1.0,
-	// back
-	-1.0, -1.0, -1.0, 1.0, 0.0, 0.0, 1.0, 0.0,
-	 1.0, -1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-	 1.0,  1.0, -1.0, 0.0, 0.0, 1.0, 0.0, 1.0,
-	-1.0,  1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0
+static float cube_vertices[] = {
+	// Front
+	-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+	 1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+	-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+	// Right
+	 1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
+	 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
+	 1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
+	 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
+	// Back
+	-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+	 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+	// Left
+	-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
+	-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
+	-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
+	-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
+	// Bottom
+	-1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
+	-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
+	 1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
+	 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
+	 // Top
+	-1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+	 1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+	 1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+	-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0
 };
 
-GLushort cube_elements[] = {
-	// front
-	0, 1, 2,
-	2, 3, 0,
-	// right
-	1, 5, 6,
-	6, 2, 1,
-	// back
-	7, 6, 5,
-	5, 4, 7,
-	// left
-	4, 0, 3,
-	3, 7, 4,
-	// bottom
-	4, 5, 1,
-	1, 0, 4,
-	// top
-	3, 2, 6,
-	6, 7, 3
+static GLushort cube_elements[] =
+{
+	 0,  1,  2,  0,  2,  3,
+	 4,  5,  6,  4,  6,  7,
+	 8,  9, 10,  8, 10, 11,
+	12, 13, 14, 12, 14, 15,
+	16, 17, 18, 16, 18, 19,
+	20, 21, 22, 20, 22, 23
 };
 
 
-int main (int argc, char** argv) {
+
+int main(int argc, char** argv) {
 	filesystem::set_current_path("content");
 
 	int result = SDL_Init(SDL_INIT_VIDEO);
@@ -58,65 +68,24 @@ int main (int argc, char** argv) {
 	VertexIndexArray vertex_array;
 	vertex_array.CreateBuffer(VertexArray::MULTI, sizeof(cube_vertices), sizeof(cube_vertices) / sizeof(GLfloat), (void*)cube_vertices);
 	vertex_array.CreateIndexBuffer(GL_UNSIGNED_SHORT, sizeof(cube_elements) / sizeof(GLushort), (void*)cube_elements);
-	vertex_array.SetAttribute(VertexArray::POSITION, 3, 8 * sizeof(GLfloat), 0);
-	vertex_array.SetAttribute(VertexArray::COLOR, 3, 8 * sizeof(GLfloat), 3 * sizeof(GLfloat));
-	vertex_array.SetAttribute(VertexArray::TEXCOORD, 2, 8 * sizeof(GLfloat), 6 * sizeof(GLfloat));
+	vertex_array.SetAttribute(VertexArray::POSITION, 3, 6 * sizeof(GLfloat), 0);
+	vertex_array.SetAttribute(VertexArray::NORMAL, 3, 6 * sizeof(GLfloat), 3 * sizeof(GLfloat));
 
 	Program program;
-	program.CompileShaderFromFile("shaders/texture.vert", GL_VERTEX_SHADER);
-	program.CompileShaderFromFile("shaders/texture.frag", GL_FRAGMENT_SHADER);
+	program.CompileShaderFromFile("shaders/basic_lit.vert", GL_VERTEX_SHADER);
+	program.CompileShaderFromFile("shaders/basic_lit.frag", GL_FRAGMENT_SHADER);
 	program.Link();
 	program.Use();
 
-	Texture texture;
-	texture.CreateTexture("textures/nc.bmp");
-	texture.Bind();
+	glm::vec3 ambient(0.3);
+	program.SetUniform("ambient", ambient);
 
-#pragma region OldTextureLoading
-	//int width, height, bpp;
+	glm::vec4 light_position(5.0, 5.0f, 5.0f, 1.0f);
 
-	//GLuint texture[2];
-	//glGenTextures(2, texture);
-
-	//glActiveTexture(GL_TEXTURE0);
-	//glBindTexture(GL_TEXTURE_2D, texture[0]);
-
-	//u8* data = Texture::LoadImage("textures/nc.bmp", width, height, bpp);
-	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
-
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	//delete data;
-
-	//glActiveTexture(GL_TEXTURE1);
-	//glBindTexture(GL_TEXTURE_2D, texture[1]);
-
-	//data = Texture::LoadImage("textures/crate.bmp", width, height, bpp);
-	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
-
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	//delete data;
-#pragma endregion
-
-	//glm::mat4 mxProjection = glm::ortho(0.0f, 800.0f, 600.0f, 0.0f, 0.01f, 1000.0f);
-	glm::mat4 mxProjection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.01f, 1000.0f);
 	glm::mat4 mxTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
 	glm::mat4 mxRotate = glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-	//glm::vec3 eye = glm::vec3(0.0f, 0.0f, 15.0f);
-	//glm::mat4 mxView = glm::lookAt(eye, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-
-	glm::mat4 mxModel = glm::mat4(1.0f);
-	//glm::mat4 mxModel = glm::scale(glm::mat4(1.0f), glm::vec3(5.0f));
-	//program.SetUniform("mx", mxModel);
+	glm::mat4 mxProjection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.01f, 1000.0f);
+	glm::mat4 mxView = glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 	bool quit = false;
 	while (!quit) {
@@ -124,17 +93,18 @@ int main (int argc, char** argv) {
 		SDL_PollEvent(&event);
 
 		switch (event.type) {
-		case SDL_QUIT: 
+		case SDL_QUIT:
 			quit = true;
-		break;
+			break;
 		case SDL_KEYDOWN:
 			if (event.key.keysym.sym == SDLK_ESCAPE) {
 				quit = true;
 			}
-		break;
+			break;
 		}
 
 		SDL_PumpEvents();
+
 		g_timer.tick();
 
 		glm::vec3 translate(0.0f, 0.0f, 0.0f);
@@ -146,19 +116,17 @@ int main (int argc, char** argv) {
 		if (input->GetKey(SDL_SCANCODE_W))		translate.z = speed;
 		if (input->GetKey(SDL_SCANCODE_S))		translate.z = -speed;
 
-		//mxModel = glm::translate(mxModel, translate * g_timer.dt());
 		mxTranslate = glm::translate(mxTranslate, translate * g_timer.dt());
 		mxRotate = glm::rotate(mxRotate, glm::radians(45.0f) * g_timer.dt(), glm::vec3(0.0f, 1.0f, 0.0f));
 		glm::mat4 mxModel = mxTranslate * mxRotate;
-		//eye = eye + translate * g_timer.dt();
-		//glm::mat4 mxView = glm::lookAt(eye, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
-		//glm::mat4 mx = mxProjection * mxView * mxModel;
-		glm::mat4 mx = mxProjection * mxModel;
-		program.SetUniform("mx", mx);
+		glm::mat4 model_view_matrix = mxView * mxModel;
+		glm::mat4 mvp_matrix = mxProjection * model_view_matrix;
 
-		//mx = glm::rotate(mx, glm::radians(90.0f) * g_timer.dt(), glm::vec3(0.0f, 0.0f, 1.0f));
-		//glUniformMatrix4fv(uniform, 1, GL_FALSE, glm::value_ptr(mx));
+		program.SetUniform("model_view_matrix", model_view_matrix);
+		program.SetUniform("mvp_matrix", mvp_matrix);
+
+		program.SetUniform("light_position", mxView * light_position);
 
 		renderer->ClearBuffer();
 
