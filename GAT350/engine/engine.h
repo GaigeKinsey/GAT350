348c65348e955ed2c9a6c2604195b91d9ab1dc40
Gaige Kinsey <gaigekinsey@yahoo.com>
2019-10-24 08:21:22 -0600

    Added the mesh loader

--- GAT350/engine/engine.h
+++ GAT350/engine/engine.h
@@ -7,6 +7,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <sstream>
 #include <string>
 #include <map>
 #include <vector>
--- GAT350/main.cpp
+++ GAT350/main.cpp
@@ -7,51 +7,7 @@
 #include "engine/renderer/texture.h"
 #include "engine/renderer/material.h"
 #include "engine/renderer/light.h"
-
-static float cube_vertices[] = {
-	// Front
-	-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-	 1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-	 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-	-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-	// Right
-	 1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
-	 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
-	 1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
-	 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
-	// Back
-	-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-	-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-	 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-	 1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-	// Left
-	-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
-	-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
-	-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
-	-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
-	// Bottom
-	-1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
-	-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
-	 1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
-	 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
-	 // Top
-	-1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-	 1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-	 1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-	-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0
-};
-
-static GLushort cube_elements[] =
-{
-	 0,  1,  2,  0,  2,  3,
-	 4,  5,  6,  4,  6,  7,
-	 8,  9, 10,  8, 10, 11,
-	12, 13, 14, 12, 14, 15,
-	16, 17, 18, 16, 18, 19,
-	20, 21, 22, 20, 22, 23
-};
-
-
+#include "engine/renderer/mesh.h"
 
 int main(int argc, char** argv) {
 	filesystem::set_current_path("content");
@@ -67,11 +23,37 @@ int main(int argc, char** argv) {
 	std::shared_ptr<Renderer> renderer = std::make_shared<Renderer>();
 	renderer->Initialize(800, 600);
 
-	VertexIndexArray vertex_array;
-	vertex_array.CreateBuffer(VertexArray::MULTI, sizeof(cube_vertices), sizeof(cube_vertices) / sizeof(GLfloat), (void*)cube_vertices);
-	vertex_array.CreateIndexBuffer(GL_UNSIGNED_SHORT, sizeof(cube_elements) / sizeof(GLushort), (void*)cube_elements);
-	vertex_array.SetAttribute(VertexArray::POSITION, 3, 6 * sizeof(GLfloat), 0);
-	vertex_array.SetAttribute(VertexArray::NORMAL, 3, 6 * sizeof(GLfloat), 3 * sizeof(GLfloat));
+
+	std::vector<glm::vec3> positions;
+	std::vector<glm::vec3> normals;
+	std::vector<glm::vec2> texcoords;
+	
+	Mesh::Load("meshes/plane.obj", positions, normals, texcoords);
+
+	VertexArray vertex_array;
+	if (!positions.empty()) {
+		vertex_array.CreateBuffer(VertexArray::POSITION, 
+			static_cast<GLsizei>(positions.size() * sizeof(glm::vec3)),
+			static_cast<GLsizei>(positions.size()), (void*)&positions[0]);
+
+		vertex_array.SetAttribute(VertexArray::POSITION, 3, 0, 0);
+	}
+
+	if (!normals.empty()) {
+		vertex_array.CreateBuffer(VertexArray::NORMAL,
+			static_cast<GLsizei>(normals.size() * sizeof(glm::vec3)),
+			static_cast<GLsizei>(normals.size()), (void*)&normals[0]);
+
+		vertex_array.SetAttribute(VertexArray::NORMAL, 3, 0, 0);
+	}
+
+	if (!texcoords.empty()) {
+		vertex_array.CreateBuffer(VertexArray::TEXCOORD,
+			static_cast<GLsizei>(texcoords.size() * sizeof(glm::vec2)),
+			static_cast<GLsizei>(texcoords.size()), (void*)&texcoords[0]);
+
+		vertex_array.SetAttribute(VertexArray::TEXCOORD, 2, 0, 0);
+	}
 
 	Material material;
 	material.program = new Program();
